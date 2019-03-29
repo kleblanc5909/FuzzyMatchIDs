d4a6c9823dfaea02399d0239d619287a8ffe6638
James LeBlanc <james.leblanc83@gmail.com>
2019-03-28 22:50:06 -0400

    limited results to 2 outcomes, created a blacklist for troublesome IDs that create false matches

--- pGUIDMatching.py
+++ pGUIDMatching.py
@@ -12,56 +12,36 @@ from fuzzywuzzy import process
 BestMatches_DtoR_array=[]
 BestMatches_RtoD_array=[]
 
-def match_names_DtoR(DAIC_IDs,All_Rutgers_IDs):
+def match_names_DtoR(DAIC_IDs,All_Rutgers_IDs,theLimit):
     for GUID in DAIC_IDs:
-        x=process.extract(GUID, All_Rutgers_IDs)
+        x=process.extract(GUID, All_Rutgers_IDs, limit=theLimit)
         BestMatches_DtoR_array.append(x)
     return BestMatches_DtoR_array
 
-def match_names_RtoD(Rutgers_IDs,All_DAIC_IDs):
+def match_names_RtoD(Rutgers_IDs,All_DAIC_IDs,theLimit):
     for GUID in Rutgers_IDs:
-        x=process.extract(GUID, All_DAIC_IDs)
+        x=process.extract(GUID, All_DAIC_IDs, limit=theLimit)
         BestMatches_RtoD_array.append(x)
     return BestMatches_RtoD_array
  
 df = pd.read_csv("pGUID_Matching.csv", engine='python')
 
-#datasets
+blackList=['NDAR_INV']
+for pattern in blackList:
+    df['AllRutgers'] = df['AllRutgers'].str.replace(pattern, '')
 
+#datasets
 Unique_DAIC_IDs = df['UniqueDAIC'].dropna()
 Unique_Rutgers_IDs = df['UniqueRutgers'].dropna()
 AllRutgersIDs = df['AllRutgers'].dropna()
 AllDAIC_IDs = df['AllDAIC']
+
+resultLimit=2
  
-BestMatches_DtoR = match_names_DtoR(Unique_DAIC_IDs,AllRutgersIDs)
-BestMatches_RtoD = match_names_RtoD(Unique_Rutgers_IDs, AllDAIC_IDs)
+BestMatches_DtoR = match_names_DtoR(Unique_DAIC_IDs,AllRutgersIDs, resultLimit)
+BestMatches_RtoD = match_names_RtoD(Unique_Rutgers_IDs, AllDAIC_IDs, resultLimit)
  
 df['BestMatchesdf_DtoR']=pd.Series(BestMatches_DtoR)
 df['BestMatchesdf_RtoD']=pd.Series(BestMatches_RtoD)
  
 df.to_csv("FuzzyMatchedIDs.csv")
-
-
-
-#NumResults = 4
-#finalResults1 = []
-#for i in range(NumResults):
-#    finalResults1.append([])
-
-#turn series from df into list 1  (short list)
-#turn series from df into parent list
-
-#design a loop over each value in list 1
-#for misPattern in listOne:
-    #   perform fuzzy check on each list 1 item
-#    tupleList = process.extract(misPattern, parentList1, limit=NumResults)
-#    
-#    for i in range(NumResults):
-#        finalResults[i].append(tupleLIst[i][1])
-
-#convert finalResults1 into 4 pd.Series
-#build new pd.DF1  that is list 1 as series + new 4 pd.Series
-
-#write out new pd.DF1
-
-#repeat ALLL for other short list (list 2) and its parent list
