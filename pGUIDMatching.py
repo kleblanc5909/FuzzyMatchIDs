b42968b84ca2f3413b11b25dc0a737b7990faa72
James LeBlanc <james.leblanc83@gmail.com>
2019-04-02 22:34:16 -0400

    Condensing the process.extract wrapper function into one function instead of two.

--- pGUIDMatching.py
+++ pGUIDMatching.py
@@ -9,21 +9,13 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-BestMatches_DtoR_array=[]
-BestMatches_RtoD_array=[]
+def match_two_collections(smallList,masterList,theLimit):
+    results=[]
+    for member in smallList:
+        x=process.extract(member, masterList, limit=theLimit)
+        results.append(x)
+    return results
 
-def match_names_DtoR(DAIC_IDs,All_Rutgers_IDs,theLimit):
-    for GUID in DAIC_IDs:
-        x=process.extract(GUID, All_Rutgers_IDs, limit=theLimit)
-        BestMatches_DtoR_array.append(x)
-    return BestMatches_DtoR_array
-
-def match_names_RtoD(Rutgers_IDs,All_DAIC_IDs,theLimit):
-    for GUID in Rutgers_IDs:
-        x=process.extract(GUID, All_DAIC_IDs, limit=theLimit)
-        BestMatches_RtoD_array.append(x)
-    return BestMatches_RtoD_array
- 
 df = pd.read_csv("pGUID_Matching.csv", engine='python')
 
 blackList=['NDAR_INV']
@@ -38,8 +30,8 @@ AllDAIC_IDs = df['AllDAIC']
 
 resultLimit=2
  
-BestMatches_DtoR = match_names_DtoR(Unique_DAIC_IDs,AllRutgersIDs, resultLimit)
-BestMatches_RtoD = match_names_RtoD(Unique_Rutgers_IDs, AllDAIC_IDs, resultLimit)
+BestMatches_DtoR = match_two_collections(Unique_DAIC_IDs,AllRutgersIDs, resultLimit)
+BestMatches_RtoD = match_two_collections(Unique_Rutgers_IDs, AllDAIC_IDs, resultLimit)
  
 df['BestMatchesdf_DtoR']=pd.Series(BestMatches_DtoR)
 df['BestMatchesdf_RtoD']=pd.Series(BestMatches_RtoD)
