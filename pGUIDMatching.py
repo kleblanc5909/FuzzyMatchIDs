06858bb3847031c4040b0966d653d1521a0a970e
James LeBlanc <james.leblanc83@gmail.com>
2019-04-09 21:20:09 -0400

    Renamed file to prepare merge to master.

--- pGUIDMatching.py
+++ pGUIDMatching.py
@@ -6,46 +6,52 @@ Created on Tue Mar 26 14:02:03 2019
 """
 
 import pandas as pd
+from pandas import ExcelWriter
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
-def match2Lists(list1,list2,theLimit):
+def match2Lists(list1,list2):
     """
     Loops over a list and returns fuzzy matches found in a second list.
     Inputs:
       list1 - list of terms to search for in the master list
       list2 - master list that is searched for matches over and over
-      theLimit - number of fuzzy match results to return
     """
-    results=[]
+    TopMatch = []
+    TopScore = []
     for member in list1:
-        x=process.extract(member, list2, limit=theLimit)
-        results.append(x)
-    return results
+        x=process.extractOne(member, list2)
+        TopMatch.append(x[0])
+        TopScore.append(x[1])
+    return TopMatch, TopScore
 
-df = pd.read_csv("pGUID_Matching.csv", engine='python')
+df = pd.read_excel("ABCD_MasterList_pGUIDs_RUIDs.xlsx")
 print ('Finished reading in input file.')
 
 blackList=['NDAR_INV']
 for pattern in blackList:
-    df['AllRutgers'] = df['AllRutgers'].replace(pattern, '')
+    df['pGUID_Rutgers'] = df['pGUID_Rutgers'].replace(pattern, '')
     
 #datasets
 Unique_DAIC_IDs = df['UniqueDAIC'].dropna()
 Unique_Rutgers_IDs = df['UniqueRutgers'].dropna()
-AllRutgersIDs = df['AllRutgers'].dropna()
-AllDAIC_IDs = df['AllDAIC']
+AllRutgersIDs = df['pGUID_Rutgers'].dropna()
+AllDAIC_IDs = df['pGUID_DAIC']
 
-resultLimit=2
+
 
 print ('About to start first match2collections.') 
-BestMatches_DtoR = match2Lists(Unique_DAIC_IDs,AllRutgersIDs, resultLimit)
+BestMatch_DtoR, BestScore_DtoR = match2Lists(Unique_DAIC_IDs,AllRutgersIDs)
 print ('Just finished first match2collections.') 
 print ('About to start second match2collections.') 
-BestMatches_RtoD = match2Lists(Unique_Rutgers_IDs, AllDAIC_IDs, resultLimit)
+BestMatch_RtoD, BestScore_RtoD = match2Lists(Unique_Rutgers_IDs, AllDAIC_IDs)
 print ('Just finished second match2collections.') 
  
-df['BestMatchesdf_DtoR']=pd.Series(BestMatches_DtoR)
-df['BestMatchesdf_RtoD']=pd.Series(BestMatches_RtoD)
+df['BestMatchdf_DtoR']=pd.Series(BestMatch_DtoR)
+df['BestScoredf_DtoR']=pd.Series(BestScore_DtoR)
+df['BestMatchdf_RtoD']=pd.Series(BestMatch_RtoD)
+df['BestScoredf_RtoD']=pd.Series(BestScore_RtoD)
  
-df.to_csv("FuzzyMatchedIDs.csv")
+writer = pd.ExcelWriter('FuzzyMatchedIDsOne.xlsx')
+df.to_excel(writer,'Sheet1')
+writer.save()
